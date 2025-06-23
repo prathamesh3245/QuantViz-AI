19b745b0f0a164b62e6a6c3926b8b90baf75986d
Prathmesh <126573912+prathamesh3245@users.noreply.github.com>
2025-06-24 00:09:29 +0530

    Added requirements and runtime needed

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn quantviz_api:app
---
+++ quantviz_api.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+import pickle
+import torch
+from gnn_fraud_detection import GCN
+
+app = Flask(__name__)
+
+model = GCN(in_channels=166, out_channels=2)
+model.load_state_dict(torch.load('model.pth'))
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    return jsonify({"fraud_probability": 0.94})
+
+@app.route('/')
+def home():
+    return "QuantViz GNN API - Send POST requests to /predict"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
