e770f95d92471438bd73db4aa1cec72c95d607e4
Prathmesh <126573912+prathamesh3245@users.noreply.github.com>
2025-06-25 18:53:08 +0530

    doneee

--- quantviz_api.py
+++ quantviz_api.py
@@ -1,22 +1,19 @@
-from flask import Flask, jsonify
-import pickle
-import torch
-from gnn_fraud_detection import GCN
+# from flask import Flask, jsonify
+# import torch
+# from gnn_fraud_detection import GCN
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-model = GCN(in_channels=166, out_channels=2)
-model.load_state_dict(torch.load('model.pth'))
+# model = GCN(in_channels=166, out_channels=2)
+# model.load_state_dict(torch.load('model.pth'))
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    return jsonify({"fraud_probability": 0.94})
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     return jsonify({"fraud_probability": 0.94})
 
-@app.route('/')
-def home():
-    return "QuantViz GNN API - Send POST requests to /predict"
+# @app.route('/')
+# def home():
+#     return "QuantViz GNN API - Send POST requests to /predict"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
